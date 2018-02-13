26b7844288a04d8e45d9be4ed16f18ba4017cdb3
Ross Hill <rosshill88@gmail.com>
2018-02-13 18:55:49 +0000

    Added pseudocode for tests

--- specs/park_spec.js
+++ specs/park_spec.js
@@ -4,11 +4,26 @@ const park = require('../park');
 
 describe('Park', function() {
 
-  let dinosaur;
-  let park;
+let dinosaur;
+let dinosaur2;
+let dinosaur3;
+let park;
 
-  beforeEach(function) {
-    dinosaur = new Dinosaur();
+  beforeEach(function() {
+    dinosaur = new Dinosaur("Stegosaurus", 4);
+    dinosaur2 = new Dinosaur("Pterodactyl", 1);
+    dinosaur3 = new Dinosaur("T-Rex", 3);
     park = new Park();
-  }
-})
+  });
+
+  // dinosaur should have a type
+  // dinosaur should have a number of offspring per year
+  // park enclosure should start empty
+  // park should be able to add a dinosaur
+  // park should be able to remove all dinosaurs of a particular type
+  // park should get all the dinosaurs with an offspring count of more than 2
+
+  it('park should start empty', function() {
+
+  });
+});
