242d8875de8e7da155fd23591b5c2cb29a23feb6
Ross Hill <rosshill88@gmail.com>
2018-02-13 20:58:23 +0000

    enclosure empty and can add dinosaur to enclosure tested

--- dinosaur.js
+++ dinosaur.js
@@ -3,4 +3,5 @@ const Dinosaur = function(type, no_of_offspring) {
   this.no_of_offspring = no_of_offspring;
 };
 
+
 module.exports = Dinosaur;
--- park.js
+++ park.js
@@ -2,4 +2,8 @@ const Park = function() {
   this.enclosure = []
 };
 
+Park.prototype.addDinosaurToEnclosure = function(dinosaur) {
+  this.enclosure.push(dinosaur);
+}
+
 module.exports = Park;
