091c4a8b6bb5ba39e44b7e742c82ec43539e46c4
Ross Hill <rosshill88@gmail.com>
2018-02-13 16:38:58 +0000

    Added constructor for test

---
+++ park.js
@@ -0,0 +1,3 @@
+const Park = function() {
+  this.enclosure = []
+}
---
+++ specs/park_spec.js
@@ -0,0 +1,14 @@
+const assert = require('assert');
+const dinosaur = require('../dinosaur');
+const park = require('../park');
+
+describe('Park', function() {
+
+  let dinosaur;
+  let park;
+
+  beforeEach(function) {
+    dinosaur = new Dinosaur();
+    park = new Park();
+  }
+})
